0470ac0462a1f2611bf6abf59d59ae2e12f6c840
Aly Abdoulaye <alyabdoulayebocoum@gmail.com>
2024-12-31 12:47:52 +0000

    Creation des routes

--- smart-pump/routes/api.php
+++ smart-pump/routes/api.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\CuveController;
+use App\Http\Controllers\PompeController;
+use App\Http\Controllers\UtilisateurController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\BonDachatController;
+use App\Http\Controllers\VenteController;
+use App\Http\Controllers\DepenseController;
+use App\Http\Controllers\FactureController;
+use App\Http\Controllers\MaintenanceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +27,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('stations', StationController::class);
+Route::apiResource('cuves', CuveController::class);
+Route::apiResource('pompes', PompeController::class);
+Route::apiResource('utilisateurs', UtilisateurController::class);
+Route::apiResource('clients', ClientController::class);
+Route::apiResource('bons-dachat', BonDachatController::class);
+Route::apiResource('ventes', VenteController::class);
+Route::apiResource('depenses', DepenseController::class);
+Route::apiResource('factures', FactureController::class);
+Route::apiResource('maintenance', MaintenanceController::class);
+
