fdbb21d631b2d9350bbfdfe28cb2352efd106990
MohamedYara <yara46403@gmail.com>
2025-01-11 14:24:31 +0100

    modification dans la table cuves

--- smart-pump/database/migrations/2024_12_25_201409_create_cuves_table.php
+++ smart-pump/database/migrations/2024_12_25_201409_create_cuves_table.php
@@ -9,14 +9,15 @@ class CreateCuvesTable extends Migration
     public function up()
     {
         Schema::create('cuves', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Clé primaire
             $table->foreignId('station_id')->constrained('stations')->onDelete('cascade'); // Relation avec stations
-            $table->decimal('capacite', 10, 2); // Capacité maximale
-            $table->decimal('niveau_actuel', 10, 2); // Niveau actuel
-            $table->decimal('seuil_minimum', 10, 2); // Seuil minimum
-            $table->string('type_carburant'); // Type de carburant (essence, diesel)
-            $table->timestamps();
+            $table->decimal('capacite', 10, 2); // Capacité maximale (en litres)
+            $table->decimal('niveau_actuel', 10, 2); // Niveau actuel (en litres)
+            $table->decimal('seuil_minimum', 10, 2); // Seuil minimum (en litres)
+            $table->foreignId('carburant_id')->constrained('carburants')->onDelete('cascade'); // Relation avec carburants
+            $table->timestamps(); // Colonnes created_at et updated_at
         });
+        
     }
 
     public function down()
